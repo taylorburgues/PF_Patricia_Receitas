7365445868decc00d67869d05d66a75325cb611e
taylorburgues <theo_xone@outlook.com>
2022-12-05 12:40:34 -0300

    alteracoes no menu

--- Escolaa/src/App.js
+++ Escolaa/src/App.js
@@ -2,7 +2,6 @@ import './App.css';
 
 import Logo from './components/template/Logo';
 import Menu from './components/template/Menu';
-import Footer from './components/template/Footer';
 import Rotas from './Rotas';
 
 import { BrowserRouter } from 'react-router-dom';
--- Escolaa/src/components/template/Footer.js
+++
@@ -1,9 +0,0 @@
-import './Footer.css'
-
-export default function Footer(props){
-    return(
-        <footer className="footer">
-            <span> .: As receitas mais gostosas do Brasil :. </span>
-        </footer>
-    )
-}
